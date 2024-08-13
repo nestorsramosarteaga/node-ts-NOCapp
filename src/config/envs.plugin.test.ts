b503a7a17e47cb86834ba61ba7332c18f9bcafdc
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-08-13 16:28:33 -0500

    Add test on envs.plugins.ts

--- src/config/envs.plugin.test.ts
+++ src/config/envs.plugin.test.ts
@@ -1,12 +1,39 @@
 import { envs } from "./envs.plugin"
 
 
-
-describe('Test envs.plugins.ts', () => {
+describe('envs.plugins.ts', () => {
 
   test('should return env options', () => {
 
-    console.log(envs);
+    expect(envs).toEqual({
+      PORT: 3000,
+      MAILER_EMAIL: 'lanki.developers@gmail.com',
+      MAILER_SECRET_KEY: 'jmepovavdhxcbgjs',
+      MAILER_SERVICE: 'gmail',
+      PROD: false,
+      MONGO_URL: 'mongodb://mongo-user:123456789@localhost:27020',
+      MONGO_DB_NAME: 'NOC-TEST',
+      MONGO_USER: 'mongo-user',
+      MONGO_PASS: '123456789',
+      POSTGRES_USER: 'postgres',
+      POSTGRES_DB: 'NOC-TEST',
+      POSTGRES_PASSWORD: '123456789'
+    });
+
+  });
+
+
+  test('should return error if not found env', async() => {
+
+    jest.resetModules();
+    process.env.PORT = 'ABCD';
+    try {
+      await import('./envs.plugin');
+      expect(true).toBe(false);
+
+    } catch (error) {
+      expect(`${error}`).toContain('"PORT" should be a valid integer');
+    }
 
   })
 
